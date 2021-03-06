8b73da2a73fb074ab384f8d38f4a47dff82bb0d6
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-03-06 01:17:25 -0500

    basic layout for html, starting js a bit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionEl = document.getElementById("questions");
+var answerEl = document.getElementById("answerButtons");
