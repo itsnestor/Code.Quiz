a5b73a559208928fd37bcf8d5cdfe5acd154efc2
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-03-06 18:57:19 -0500

    added timer and start quiz functions. added hide class for css

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,45 @@ var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
 var questionEl = document.getElementById("questions");
 var answerEl = document.getElementById("answerButtons");
+var timerEl = document.getElementById("timer");
+var home = document.getElementById("home");
+var quiz = document.getElementById("quiz");
+var submitScore = document.getElementById("submitScore")
+
+var timerLeft = 100;
+var shuffleQuestions, currentQuestion, quizTime, userName;
+var rightAnswers = 0;
+
+// getting start button to work to start quiz
+startButton.addEventListener("click", startQuiz);
+// getting next button to work
+nextButton.addEventListener("click", () => {
+    currentQuestion++
+
+})
+// starting the quiz
+function startQuiz() {
+    shuffleQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestion = 0;
+    rightAnswers = 0;
+    home.classList.add("hide");
+    quiz.classList.remove("hide");
+
+    timeRemain();
+
+}
+
+// setting up timer
+function timeRemain () {
+    quizTime = setInterval(() => {
+        if (timerLeft > 0) {
+            timerEl.textContent = timerLeft;
+            timerLeft--;
+        } else if (timerLeft === 0) {
+            timerEl.textContent = timerLeft;
+            timerLeft--;
+        } else {
+            endQuiz();
+        }
+    }, 1000);
+}
