a020b92ee2f9bf18b42b40d402cc56b4418060d0
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-03-06 19:42:09 -0500

    adding quiz functionality, readded question.js for visability

---
+++ assets/question.js
@@ -0,0 +1,51 @@
+var question = [
+    {
+        question: "Where is my question?",
+        answer: [
+            { text: "Answer 1", correct: false},
+            { text: "Answer 2", correct: false},
+            { text: "Answer 3", correct: true},
+            { text: "Answer 4", correct: false},
+        ]
+    },
+
+    {
+        question: "Where is my question?",
+        answer: [
+            { text: "Answer 1", correct: false},
+            { text: "Answer 2", correct: false},
+            { text: "Answer 3", correct: true},
+            { text: "Answer 4", correct: false},
+        ]
+    },
+
+    {
+        question: "Where is my question?",
+        answer: [
+            { text: "Answer 1", correct: false},
+            { text: "Answer 2", correct: false},
+            { text: "Answer 3", correct: true},
+            { text: "Answer 4", correct: false},
+        ]
+    },
+
+    {
+        question: "Where is my question?",
+        answer: [
+            { text: "Answer 1", correct: false},
+            { text: "Answer 2", correct: false},
+            { text: "Answer 3", correct: true},
+            { text: "Answer 4", correct: false},
+        ]
+    },
+
+    {
+        question: "Where is my question?",
+        answer: [
+            { text: "Answer 1", correct: false},
+            { text: "Answer 2", correct: false},
+            { text: "Answer 3", correct: true},
+            { text: "Answer 4", correct: false},
+        ]
+    },
+];
--- assets/script.js
+++ assets/script.js
@@ -10,76 +10,25 @@ var submitScore = document.getElementById("submitScore")
 var timerLeft = 100;
 var shuffleQuestions, currentQuestion, quizTime, userName;
 var rightAnswers = 0;
-var questions = [
-    {
-        question: "Where is my question?",
-        answers: [
-            { text: "Answer 1", correct: false}
-            { text: "Answer 2", correct: false}
-            { text: "Answer 3", correct: true}
-            { text: "Answer 4", correct: false}
-        ],
-    },
-
-    {
-        question: "Where is my question?",
-        answers: [
-            { text: "Answer 1", correct: false}
-            { text: "Answer 2", correct: false}
-            { text: "Answer 3", correct: true}
-            { text: "Answer 4", correct: false}
-        ],
-    },
-
-    {
-        question: "Where is my question?",
-        answers: [
-            { text: "Answer 1", correct: false}
-            { text: "Answer 2", correct: false}
-            { text: "Answer 3", correct: true}
-            { text: "Answer 4", correct: false}
-        ],
-    },
-
-    {
-        question: "Where is my question?",
-        answers: [
-            { text: "Answer 1", correct: false}
-            { text: "Answer 2", correct: false}
-            { text: "Answer 3", correct: true}
-            { text: "Answer 4", correct: false}
-        ],
-    },
-
-    {
-        question: "Where is my question?",
-        answers: [
-            { text: "Answer 1", correct: false}
-            { text: "Answer 2", correct: false}
-            { text: "Answer 3", correct: true}
-            { text: "Answer 4", correct: false}
-        ],
-    },
-];
 
 // getting start button to work to start quiz
 startButton.addEventListener("click", startQuiz);
 
 // getting next button to work
 nextButton.addEventListener("click", () => {
-    currentQuestion++
-
+    currentQuestion++;
+    nextQuestion();
 })
 // starting the quiz
 function startQuiz() {
-    shuffleQuestions = questions.sort(() => Math.random() - 0.5);
+    shuffleQuestions = question.sort(() => Math.random() - 0.5);
     currentQuestion = 0;
     rightAnswers = 0;
     home.classList.add("hide");
     quiz.classList.remove("hide");
-
+    
     timeRemain();
-
+    nextQuestion();
 }
 
 // setting up timer
@@ -97,14 +46,79 @@ function timeRemain () {
     }, 1000);
 }
 
-function showQuestion(questions) {
-
+function showQuestion(question) {
+    questionEl.innerHTML = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerEl.appendChild(button);
+    })
 }
-
-
 
 // next question
 function nextQuestion () {
     resetState();
     showQuestion(shuffleQuestions[currentQuestion]);
+}
+
+// resets the quiz to default state
+function resetState () {
+    clearStatusClass(document.querySelector("main"));
+    nextButton.classList.add("hide");
+    while (answerEl.firstChild){
+        answerEl.removeChild(answerEl.firstChild);
+    }
+}
+
+// choosing answer
+function chooseAnswer(x) {
+    // users selected answer
+    var selectedAnswer = x.target;
+    var correct = selectedAnswer.dataset.correct;
+    setStatusClass(document.querySelector("main"), correct);
+    if (!correct) {
+        timerLeft -= 20;
+    }
+    // checks other buttons to see if correct
+    Array.from(answerEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffleQuestions.length > currentQuestion +1) {
+        nextButton.classList.remove("hide");
+    }
+    else {
+        endQuiz();
+    }
+    if (selectedAnswer.dataset = correct) {
+        rightAnswers += 20;
+    }
+    document.getElementById("rightAnswers").innerHTML = rightAnswers;
+}
+
+// checks to see if answer is right or wrong
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct");
+    }
+    else {
+        element.classList.add("wrong")
+    }
+}
+
+// clears right/wrong from answers
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+}
+
+function endQuiz() {
+    quiz.classList.add("hide");
+    submitScore.classList.remove("hide");
+    clearInterval(quizTime);
 }
